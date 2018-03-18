3e8d748d17ed411e7ea5ae0ee69f29f094477e40
Scott-Ayers <scottasba@hotmail.com>
2018-03-18 13:55:37 -0400

    Add files via upload

---
+++ res/c4d_symbols.h
@@ -0,0 +1,11 @@
+enum
+{
+	// string table definitions
+	IDS_GRADIENTTITLE	= 10000, // Starts out the enum assignment process at this numerical value
+	IDS_My_Simple_Plugin,   // The plugin's numerical ID that lets C4D register it
+	                        // There must be a corresponding res\strings_us\.str file with same ID name for text to show up in the menu
+	
+
+// End of symbol definition
+  _MY_DUMMY_ELEMENT_
+};
